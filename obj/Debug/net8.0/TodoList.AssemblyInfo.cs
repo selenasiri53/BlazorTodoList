ad0f441f3c763ba3ac1de73fff34ac32d8387835
selenasiri53 <selenasiri53@gmail.com>
2024-01-18 17:32:03 -0500

    added checkbox and todo item

--- obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e30726cdca8b5d6c24ebbc3b71f9846b97e7b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ab77d4627afb2f1a3548dec066eff535a125f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
